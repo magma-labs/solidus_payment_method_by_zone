0e93bb5bd65cc525bc5a6b39f8a9af5323e232f9
César Carruitero <ccarruitero@protonmail.com>
2020-09-10 17:18:22 -0500

    fix order#available_payment_methods

--- app/decorators/models/solidus_payment_method_by_zone/spree/order_decorator.rb
+++ app/decorators/models/solidus_payment_method_by_zone/spree/order_decorator.rb
@@ -2,9 +2,9 @@ module SolidusPaymentMethodByZone
   module Spree
     module OrderDecorator
       def available_payment_methods
-        @available_payment_methods ||= super.left_joins(:payment_method_zone)
+        @available_payment_methods ||= super.left_joins(:payment_method_zones)
              .where(spree_payment_method_zones: {
-                 zone_id: [nil] + ::Spree::Zone.for_address(ship_address).pluck(&:id)
+                 zone_id: [nil] + ::Spree::Zone.for_address(ship_address).pluck(:id)
              })
       end
 
--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -4,16 +4,19 @@ require 'spec_helper'
 
 describe Spree::Order, type: :model do
   let(:mx) { create(:country_mx) }
+  let(:usa) { Spree::Country.find_by(iso: 'US') }
   let(:us_zone) { create(:zone) }
   let(:mx_zone) { create(:zone) }
-  let(:us_payment_method) { create(:check_payment_method,
-                                              name: 'US method') }
-  let(:mx_payment_method) { create(:check_payment_method,
-                                              name: 'MX method') }
+  let(:us_payment_method) { create(:check_payment_method, name: 'US method') }
+  let(:mx_payment_method) { create(:check_payment_method, name: 'MX method') }
   let(:order) { create(:order_with_totals) }
 
   before(:each) do
     order.line_items << create(:line_item)
+    us_zone.members.create(zoneable: usa)
+    mx_zone.members.create(zoneable: mx)
+    us_payment_method.payment_method_zones.create(zone: us_zone)
+    mx_payment_method.payment_method_zones.create(zone: mx_zone)
   end
 
   describe 'available_payment_methods' do
@@ -28,12 +31,33 @@ describe Spree::Order, type: :model do
     end
 
     it 'must not include payment methods for Mexico in addresses of US' do
-      us_zone.members.create(zoneable: Spree::Country.find_by(iso: 'US'))
-      us_payment_method.zones << us_zone
-      mx_zone.members.create(zoneable: mx)
-      mx_payment_method.zones << mx_zone
       expect(order.available_payment_methods).to include(us_payment_method)
       expect(order.available_payment_methods).not_to include(mx_payment_method)
+    end
+
+    context 'with mexican address' do
+      let(:state) { create(:state_ja, country: mx) }
+      let(:address) { create(:address, city: 'Leon', state: state) }
+      let!(:order) { create(:order_with_totals, ship_address: address) }
+
+      it 'must include mx payment method' do
+        expect(order.available_payment_methods).not_to include(us_payment_method)
+        expect(order.available_payment_methods).to include(mx_payment_method)
+      end
+
+      context 'with state zone' do
+      end
+    end
+
+    context 'with address not matching zones' do
+      let(:ar) { create(:country, iso: 'AR') }
+      let(:caba) { create(:state_ja, country: ar, state_code: 'C') }
+      let(:address) { create(:address, city: 'Recoleta', state: caba) }
+      let!(:order) { create(:order_with_totals, ship_address: address) }
+
+      it 'must not return any payment method' do
+        expect(order.available_payment_methods.count).to eq(0)
+      end
     end
   end
 end
