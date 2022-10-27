bd83e1d56cb94cb97554683a336ea40e56108793
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2022-10-27 15:31:32 -0500

    Refactor decorator files

--- app/decorators/solidus_payment_method_by_zone/spree/order_decorator.rb
+++ app/decorators/solidus_payment_method_by_zone/spree/order_decorator.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-module SolidusPaymentMethodByZone
-  module Spree
-    module OrderDecorator
-      def available_payment_methods
-        @available_payment_methods ||= super.available_to_address(ship_address)
+module Spree
+  module OrderDecorator
+    def self.prepended(base)
+      base.class_eval do
+        def available_payment_methods
+          @available_payment_methods ||= super.available_to_address(ship_address)
+        end
       end
-
-      ::Spree::Order.prepend self
     end
+
+    ::Spree::Order.prepend(self)
   end
 end
--- app/decorators/solidus_payment_method_by_zone/spree/zone_decorator.rb
+++ app/decorators/solidus_payment_method_by_zone/spree/zone_decorator.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
-module SolidusPaymentMethodByZone
-  module Spree
-    module ZoneDecorator
-      extend ActiveSupport::Concern
-
-      included do
-        has_and_belongs_to_many :payment_methods, join_table: 'spree_payment_method_zones'
-      end
+module Spree
+  module ZoneDecorator
+    def self.prepended(base)
+      base.has_and_belongs_to_many :payment_methods,
+                                   join_table: 'spree_payment_method_zones'
     end
+
+    ::Spree::Zone.prepend(self)
   end
 end
-
-Spree::PaymentMethod.include SolidusPaymentMethodByZone::Spree::ZoneDecorator
