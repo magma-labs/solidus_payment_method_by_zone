ef6edee6406c0254abd4d885e01ebbe3288bf143
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2023-02-28 14:08:28 -0600

    Fix rubocop warnings

--- app/decorators/solidus_payment_method_by_zone/spree/zone_decorator.rb
+++ app/decorators/solidus_payment_method_by_zone/spree/zone_decorator.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
+<<<<<<< Updated upstream
 module SolidusPaymentMethodByZone
   module Spree
     module ZoneDecorator
       extend ActiveSupport::Concern
+=======
+module Spree
+  module ZoneDecorator
+    def self.prepended(base)
+      base.has_and_belongs_to_many :payment_methods, join_table: 'spree_payment_method_zones'
+    end
+>>>>>>> Stashed changes
 
       included do
         has_and_belongs_to_many :payment_methods, join_table: 'spree_payment_method_zones'
--- lib/solidus_payment_method_by_zone.rb
+++ lib/solidus_payment_method_by_zone.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 require 'solidus_core'
+require 'solidus_backend'
 require 'solidus_support'
 require 'deface'
+
+require 'solidus_payment_method_by_zone/version'
 require 'solidus_payment_method_by_zone/engine'
